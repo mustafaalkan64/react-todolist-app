f07ab83f98e9ddc6df03f80360a4e96d14c10bd7
mustafaalkan64 <mustafaalkan64@gmail.com>
2020-04-21 02:22:45 +0300

    Removed unneccesary files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "alertifyjs/build/css/alertify.min.css";
